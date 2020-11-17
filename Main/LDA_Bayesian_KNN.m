a363eed37fe7dca26f0d14f9019e651c54ca7dc7
AmoghavarshaP <aprasann@terpmail.umd.edu>
2020-11-17 12:31:26 -0500

    Adding modified files

---
+++ Main/LDA_Bayesian_KNN.m
@@ -0,0 +1,38 @@
+clc;
+clear;
+close all;
+data_set = 'data.mat';
+%Cropped set of images is 200 subjects
+data_size = 200;
+%Test-train split 50-50 split
+%try 0.25,0.75
+data_split = 0.5;
+K=7;
+%Extract Training and Testing data
+training_data = get_data('train',data_set,data_size,data_split);
+testing_data = get_data('test',data_set,data_size,data_split);
+
+%Defining X_train, X_train to include both classes from the training and testing data resplecively
+%X_train = [x_neutral x_expression]; X_test = [x_neutral x_expression]
+x_train = [training_data(:,1:3:3*data_size*data_split) training_data(:,2:3:3*data_size*data_split)];
+x_test = [testing_data(:,1:3:3*data_size*(1-data_split)) testing_data(:,2:3:3*data_size*(1-data_split))];
+
+%To calculate the mean value for the neutral and expression classes
+neutral_mu = sum(training_data(:,1:3:3*data_size*data_split),2)/size(training_data(:,1:3:3*data_size*data_split),2);
+expression_mu = sum(training_data(:,2:3:3*data_size*data_split),2)/size(training_data(:,2:3:3*data_size*data_split),2);
+
+%To calculate the covariances for the both the classes and get the total
+neutral_cov = cov(training_data(:,1:3:3*data_size*data_split)');
+expression_cov = cov(training_data(:,2:3:3*data_size*data_split)');
+
+total_cov = neutral_cov + expression_cov;
+
+%Inverse of the total covariance 
+inv_total_cov = pinv(total_cov);
+
+%Direction of projection or theta for the data
+theta = inv_total_cov.*(neutral_mu-expression_mu);
+%Calling Bayes function
+bayes_function(training_data,testing_data,1,theta,data_size,data_split);
+%Calling KNN Function
+KNN_function(training_data,testing_data,K,theta,data_size,data_split);
---
+++ Main/PCA_Bayesian_KNN.m
@@ -0,0 +1,24 @@
+clc;
+clear;
+close all;
+
+data_set = 'data.mat';
+%Cropped set of images is 200 subjects
+data_size = 200;
+%Test-train split 50-50
+data_split = 0.75;
+
+%Extract Training and Testing data
+training_data = get_data('train',data_set,data_size,data_split);
+testing_data = get_data('test',data_set,data_size,data_split);
+
+%%Performing PCA on the data:
+[U,S,V] = svds(training_data,25);
+%%Calling the bayes function:
+delta = 1;
+bayes_function(training_data,testing_data,delta,U,data_size,data_split);
+%%Calling the KNN function 
+K=3;
+KNN_function(training_data,testing_data,K,U,data_size,data_split);
+
+
